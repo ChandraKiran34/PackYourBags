a12569a4e3dc91e6285dcd02e5ac4c15e1a4b9bf
ChandraKiran34 <bchakri06@gmail.com>
2024-05-02 09:44:44 +0530

    cors changed2

--- back-end/server.js
+++ back-end/server.js
@@ -33,7 +33,7 @@ app.use(morgan('common'));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors({
-    origin: ["http://localhost:3000","http://3.108.54.68:3000","https://pack-your-bags-three.vercel.app/"],
+    origin: ["http://localhost:3000","https://pack-your-bags-three.vercel.app"],
     methods: ["POST", "GET", "HEAD", "PUT", "DELETE"],
     credentials: true,
   }));
